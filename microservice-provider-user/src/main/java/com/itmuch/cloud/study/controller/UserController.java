e5683ace3dc9085bfddc321c39b93c4011de2f16
HuMaohua <769623301@qq.com>
2017-12-26 22:38:52 +0800

    测试springboot的日志级别

--- microservice-provider-user/src/main/java/com/itmuch/cloud/study/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/study/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.itmuch.cloud.study.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +12,15 @@ import com.itmuch.cloud.study.repository.UserRepository;
 
 @RestController
 public class UserController {
+  private static final Logger logger = LoggerFactory.getLogger(UserController.class);
   @Autowired
   private UserRepository userRepository;
 
   @GetMapping("/{id}")
   public User findById(@PathVariable Long id) {
+    logger.trace("测试springboot日志级别。。。");
+    logger.info("测试springboot日志级别。。。");
+    logger.warn("测试springboot日志级别。。。");
     User findOne = this.userRepository.findOne(id);
     return findOne;
   }
